2ca2a04718cf9660c9a203ea8f73b2eb6e9658d2
Sour-2304 <ponomarenko2304@yandex.ru>
2023-12-02 21:52:47 +0300

    Sounds added but soldier isn`t moving.

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import pygame
+from pygame import mixer
 import math
+
+
+def sound():
+    mixer.init()
+    mixer.music.load("assets/sounds/shoot.mp3")
+    mixer.music.play()
 
 
 pygame.init()
@@ -13,10 +20,11 @@ screen = pygame.display.set_mode([WIDTH, HEIGHT])
 BG = pygame.image.load("assets/bg.jpg")
 banners = []
 poses = []
-copters = [pygame.image.load(f'assets/copters/fpvC.png'), pygame.image.load(f'assets/copters/grenadeC.jpg')]
+copters = [pygame.image.load(f'assets/copters/fpvC.png'), pygame.image.load(f'assets/copters/grenadeC.png')]
 sd_pos = [(WIDTH/2-75, HEIGHT-240), (WIDTH/2-87, HEIGHT-210), (WIDTH/2-45, HEIGHT-240), (WIDTH/2-100, HEIGHT-210)]
 level = 1
 curr_move = 1
+shoot_time = 0
 
 for i in range(1, 5):
     poses.append(pygame.image.load(f'assets/poses/sd{i}.png'))
@@ -29,6 +37,8 @@ while run:
     screen.fill('black')
     screen.blit(BG, (0, 0))
 
+    shoot = False
+
     pressed = pygame.key.get_pressed()
     if pressed[pygame.K_w] and str(curr_move) in '13':
         curr_move += 1
@@ -38,6 +48,19 @@ while run:
         curr_move += 2
     if pressed[pygame.K_a] and str(curr_move) in '34':
         curr_move -= 2
+    if pressed[pygame.K_SPACE]:
+        if shoot_time != 0:
+            passed_time = pygame.time.get_ticks() - shoot_time
+            if passed_time < 500:
+                screen.blit(pygame.image.load("assets/icons/reload.png"), (860, 5))
+            else:
+                shoot = True
+                shoot_time = pygame.time.get_ticks()
+                sound()
+        else:
+            shoot = True
+            shoot_time = pygame.time.get_ticks()
+            sound()
 
     screen.blit(poses[curr_move - 1], sd_pos[curr_move - 1])
 
