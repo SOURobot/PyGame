e1eae2b133c9a96c65793b4238e0a9324a121725
Sour-2304 <ponomarenko2304@yandex.ru>
2023-12-02 18:06:16 +0300

    Copters` models added.

--- main.py
+++ main.py
@@ -13,9 +13,10 @@ screen = pygame.display.set_mode([WIDTH, HEIGHT])
 BG = pygame.image.load("assets/bg.jpg")
 banners = []
 poses = []
+copters = [pygame.image.load(f'assets/copters/fpvC.png'), pygame.image.load(f'assets/copters/grenadeC.jpg')]
+sd_pos = [(WIDTH/2-75, HEIGHT-240), (WIDTH/2-87, HEIGHT-210), (WIDTH/2-45, HEIGHT-240), (WIDTH/2-100, HEIGHT-210)]
 level = 1
 curr_move = 1
-sd_pos = [(WIDTH/2-75, HEIGHT-240), (WIDTH/2-100, HEIGHT-210), (WIDTH/2-45, HEIGHT-240), (WIDTH/2-100, HEIGHT-210)]
 
 for i in range(1, 5):
     poses.append(pygame.image.load(f'assets/poses/sd{i}.png'))
@@ -29,13 +30,13 @@ while run:
     screen.blit(BG, (0, 0))
 
     pressed = pygame.key.get_pressed()
-    if pressed[pygame.K_UP] and str(curr_move) in '13':
+    if pressed[pygame.K_w] and str(curr_move) in '13':
         curr_move += 1
-    if pressed[pygame.K_DOWN] and str(curr_move) in '24':
+    if pressed[pygame.K_s] and str(curr_move) in '24':
         curr_move -= 1
-    if pressed[pygame.K_RIGHT] and str(curr_move) in '12':
+    if pressed[pygame.K_d] and str(curr_move) in '12':
         curr_move += 2
-    if pressed[pygame.K_LEFT] and str(curr_move) in '34':
+    if pressed[pygame.K_a] and str(curr_move) in '34':
         curr_move -= 2
 
     screen.blit(poses[curr_move - 1], sd_pos[curr_move - 1])
