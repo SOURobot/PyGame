522f1db71f24ec2d542632a7d766ab74238b8c67
Sour-2304 <ponomarenko2304@yandex.ru>
2023-12-17 11:53:51 +0300

    Added a lot

--- egg_fall.py
+++ egg_fall.py
@@ -1,16 +1,20 @@
 import os
 import random
 import sys
-
 import pygame
+from sounds import *
 
-size = width, height = 600, 900
+
+size = width, height = 500, 700
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption('EGG_FALL')
 clock = pygame.time.Clock()
 
 level = 1
 speed = 6
+score = 0
+pan_score = 0
+health = 3
 
 
 def load_image(name, colorkey=None):
@@ -41,8 +45,16 @@ class Egg(pygame.sprite.Sprite):
 
     def update(self, *args):
         self.rect = self.rect.move(0, speed)
-        if args and args[0].type == pygame.MOUSEBUTTONDOWN and self.rect.collidepoint(args[0].pos) or self.rect.top > height - 45:
+        if pygame.sprite.collide_mask(self, pan):
+            global pan_score
+            catch()
             self.kill()
+            pan_score += 1
+        elif self.rect.top > 655:
+            global health
+            miss()
+            self.kill()
+            health -= 1
 
 
 class Pan(pygame.sprite.Sprite):
@@ -52,11 +64,35 @@ class Pan(pygame.sprite.Sprite):
         super().__init__(*group)
         self.image = Pan.image
         self.rect = self.image.get_rect()
-        self.rect.x = 235
-        self.rect.y = 700
+        self.rect.x = 185
+        self.rect.y = 580
+
+    def burn(self):
+        global score, pan_score
+        fire()
+        score += pan_score
+        pan_score = 0
 
     def update(self, *args):
-        pass
+        if args:
+            if event.key == pygame.K_a:
+                self.rect.x = max(0, self.rect.x - 40)
+            if event.key == pygame.K_s:
+                self.burn()
+            if event.key == pygame.K_d:
+                self.rect.x = min(370, self.rect.x + 40)
+
+
+p_s_im = load_image("pan_score.png")
+s_im = load_image("fried_egg.png")
+h_p = load_image("health_point.png")
+
+
+def draw_info():
+    screen.blit(p_s_im, (0, height - 50))
+    screen.blit(s_im, (0, 0))
+    for i in range(health):
+        screen.blit(h_p, (width - 50, i * 50))
 
 
 all_sprites = pygame.sprite.Group()
@@ -69,6 +105,8 @@ for i in range(5):
     all_sprites.add(e)
     eggs.add(e)
 
+
+# main_theme()
 running = True
 while running:
     clock.tick(30)
@@ -76,10 +114,11 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        if event.type == pygame.MOUSEMOTION:
-            all_sprites.update(event)
+        if event.type == pygame.KEYDOWN:
+            pan.update(event)
 
     screen.fill(pygame.Color("black"))
+    draw_info()
     all_sprites.draw(screen)
     pygame.display.flip()
 
---
+++ sounds.py
@@ -0,0 +1,25 @@
+from pygame import mixer
+
+
+def fire():
+    mixer.init()
+    mixer.music.load("egg_fall/sounds/fire.mp3")
+    mixer.music.play()
+
+
+def catch():
+    mixer.init()
+    mixer.music.load("egg_fall/sounds/catch.mp3")
+    mixer.music.play()
+
+
+def miss():
+    mixer.init()
+    mixer.music.load("egg_fall/sounds/miss.mp3")
+    mixer.music.play()
+
+
+def main_theme():
+    mixer.init()
+    mixer.music.load("egg_fall/sounds/main_theme.mp3")
+    mixer.music.play()
