e94e500760b08db0eaf971e53291ff8296f9a042
Sour-2304 <ponomarenko2304@yandex.ru>
2024-01-14 19:06:42 +0300

    ADDED ALL

--- main.py
+++ main.py
@@ -86,6 +86,29 @@ class Pan(pygame.sprite.Sprite):
                 self.rect.x = min(370, self.rect.x + 40)
 
 
+class AnimatedSprite(pygame.sprite.Sprite):
+    def __init__(self, sheet, columns, rows, x, y):
+        super().__init__(all_sprites)
+        self.frames = []
+        self.cut_sheet(sheet, columns, rows)
+        self.cur_frame = 0
+        self.image = self.frames[self.cur_frame]
+        self.rect = self.rect.move(x, y)
+
+    def cut_sheet(self, sheet, columns, rows):
+        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
+                                sheet.get_height() // rows)
+        for j in range(rows):
+            for i in range(columns):
+                frame_location = (self.rect.w * i, self.rect.h * j)
+                self.frames.append(sheet.subsurface(pygame.Rect(
+                    frame_location, self.rect.size)))
+
+    def update(self):
+        self.cur_frame = (self.cur_frame + 1) % len(self.frames)
+        self.image = self.frames[self.cur_frame]
+
+
 def draw_menu():
     screen.blit(icon, (20, 50))
     screen.blit(menu_font.render("EGG FALL", True, (255, 201, 92)), [125, 50])
@@ -173,6 +196,8 @@ all_sprites = pygame.sprite.Group()
 eggs = pygame.sprite.Group()
 pan = Pan()
 all_sprites.add(pan)
+flame = AnimatedSprite(load_image("flame.png"), 8, 4, 50, 50)
+all_sprites.add(flame)
 
 
 def survive():
